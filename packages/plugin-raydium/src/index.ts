809fa2eb68338e48dd19535f6b1e6da17f395912
choguun <visaruth.s@gmail.com>
2024-12-13 01:43:22 +0700

    add: raydium plugin

---
+++ packages/plugin-raydium/src/index.ts
@@ -0,0 +1,11 @@
+import {
+    Plugin
+} from "@ai16z/eliza";
+
+export const raydiumPlugin: Plugin = {
+    name: "Raydium",
+    description: "Raydium plugin",
+    actions: [],
+    evaluators: [],
+    providers: [],
+}
